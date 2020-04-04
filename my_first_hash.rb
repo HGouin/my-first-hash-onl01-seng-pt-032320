0c4057aeb39aa0877a0944f93322b654c474e259
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-04-04 05:52:30 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -24,7 +24,9 @@ def adding
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
+  shipping_manifest["musket"] = 2
+  shipping_manifest["gun powder"] = 4
+  shipping_manifest# add 2 muskets to the shipping_manifest hash below
 
 
 
